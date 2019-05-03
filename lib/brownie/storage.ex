56743f0376adfac897098aeed7d8c0066632eaac
mopp <hello@mopp.jp>
2019-05-03 23:47:27 +0900

    feat: define public interface

--- lib/brownie/storage.ex
+++ lib/brownie/storage.ex
@@ -7,4 +7,20 @@ defmodule Brownie.Storage do
   @callback read(key()) :: {:ok, value()} | {:error, reason()}
   @callback update(key(), value()) :: :ok | {:error, reason()}
   @callback delete(key()) :: :ok | {:error, reason()}
+
+  def create(key, value) do
+    Brownie.Application.get_backend().create(key, value)
+  end
+
+  def read(key) do
+    Brownie.Application.get_backend().read(key)
+  end
+
+  def update(key, value) do
+    Brownie.Application.get_backend().update(key, value)
+  end
+
+  def delete(key) do
+    Brownie.Application.get_backend().delete(key)
+  end
 end
